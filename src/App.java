9f97f66bb7cd5376d26658a8ecf2983f5826b8cb
Ostapiak Mykhailo <ostapiakmz@gmail.com>
2026-01-15 10:59:25 +0200

    Complete project

--- src/App.java
+++ src/App.java
@@ -2,23 +2,14 @@ import controllers.ProductController;
 import models.Product;
 import views.SalesView;
 
-// Входная точка в программу/приложение
 public class App {
 
     public static void main(String[] args) {
 
-        // Здесь создайте экземпляры Model, View и Controller,
-        // на основании соответствующих конструкторов.
-//        Product model = создать экземпляр
         Product model = new Product();
-//        SalesView view = создать экземпляр
         SalesView view = new SalesView(model);
-//        ProductController controller = создать экземпляр
         ProductController controller = new ProductController(model, view);
 
-        // Запуск программы/приложения.
-        // Раскомментированный код будет корректным после
-        // правильной настройки приложения
         controller.runApp();
     }
 }
--- src/controllers/ProductController.java
+++ src/controllers/ProductController.java
@@ -12,30 +12,22 @@ public class ProductController {
     Product model;
     SalesView view;
 
-    // Конструктор
     public ProductController(Product model, SalesView view) {
         this.model = model;
         this.view = view;
     }
 
     public void runApp() {
-
         view.getInputs();
-
-        // 1) получение имени товара через модель;
         String productName = model.getName();
 
-        // 2) вызов методов расчетов доходов и налога;
         double income = model.calculateIncome(model.getQuantity(), model.getPrice());
         double tax = model.calculateIncomeTax(income);
         double netIncome = model.calculateIncome(income, tax);
 
-        // 3) округление расчетных значений;
         BigDecimal incomeRounded = Rounder.roundSum(income);
         BigDecimal taxRounded = Rounder.roundSum(tax);
         BigDecimal netIncomeRounded = Rounder.roundSum(netIncome);
-
-        // 4) вывод расчетов по заданному формату.
 
         String output = "Product name: " + productName + "\n" +
                 "Total income: " + incomeRounded + "\n" +
